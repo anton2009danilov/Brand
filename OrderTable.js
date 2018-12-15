47895159cfdfe8722fc61738e4757a1d33b7e45f
Anton Danilov <anton2009danilov@yandex.ru>
2018-12-16 01:11:09 +0300

    таблица заказа: частично сделана первая колонка товара "Product details"

--- OrderTable.js
+++ OrderTable.js
@@ -108,7 +108,7 @@ class OrderTable {
     let $sectionThree = $('<section class="orderForms-section orderForms-proceedToCheck"></section>');
     let $sectionThree_paragraph = $(`<p class="orderForms-proceedToCheck-text">
             Sub total
-            <span class="orderForms-proceedToCheck-markedText" id="totalSum">$${this.subtotalSum}</span> 
+            <span class="orderForms-proceedToCheck-markedText" id="subtotalSum">$${this.subtotalSum}</span> 
         </p>`);
     // let $subtotalSum = (`$<span class="orderForms-markedText" id="totalSum">$${this.subtotalSum}</span>`);
     let $sectionThree_header = $(`<header class="orderForms-sectionHeader orderForms-CheckOutHeader">
@@ -127,26 +127,96 @@ class OrderTable {
 
   }
 
+  _renderSum() {
+    // $('.sum-amount').text(`Всего товаров в корзине: ${this.countGoods}`);
+    $('#subtotalSum').text(`$${Math.abs((this.subtotalSum).toFixed(2))}`);
+    $('#totalSum').text(`$${Math.abs((this.amount).toFixed(2))}`);
+  }
+
+  _renderItem(product) {
+    // let $container = $('.orderTable-container');
+    let $container = $('.orderTable-itemsWrapper');
+
+    let $tableRow = $(`<div class="orderTable-row" data-product="${product.id_product}"></div>`);
+    let $column1 = $('<div class="orderTable-columnOne"></div>');
+    let $productLink = $(`<a class="orderTable-itemCard" href="${product.href}"></a>`);
+    let $productImg = $(`<img src="${product.src}" alt="itemCard-photo${product.id_product}" class="itemCard-img">`);
+    let $textBox = $('<div class="itemCard-textBlock"></div>');
+    let $productName = $(`<header class="itemCard-header">${product.product_name}</header>`);
+    let $stars = $(`<div class="itemCard-stars"></div>`);
+    for (let i = 1; i <= product["full_star"]; i++) {
+      let $fullStar = $('<i class="fas fa-star">');
+      $fullStar.appendTo($stars);
+    }
+
+    if (product["half_star"]) {
+      let $halfStar = $('<i class="fas fa-star-half-alt">');
+      $halfStar.appendTo($stars);
+    }
+
+    if (product["full_star"] < 5 && !product["half_star"]) {
+      for (let i = 0; i < 5 - product["full_star"]; i++) {
+        let $emptyStar = $('<i class="far fa-star">');
+        $emptyStar.appendTo($stars);
+      }
+    }
+
+    let $color = $(`<p class="itemCard-text">color: ${product.color}</p>`);
+
+    let $column2 = $('<div class="orderTable-column"></div>');
+    let $column3 = $('<div class="orderTable-column"></div>');
+    let $column4 = $('<div class="orderTable-column"></div>');
+    let $column5 = $('<div class="orderTable-column"></div>');
+    let $column6 = $('<div class="orderTable-lastColumn"></div>');
+
+
+
+    let $productQuantityAndPrice = $(
+      `<p class="cart-ordered">
+    <span class="cart-productQuantity">${product.quantity}</span> <span class="cart-ordered-x">x</span>
+     $<span class="cart-productPrice">${product.price}</span></p>`
+    );
+    let $delBtn = $(`<button class="cart-deleteButton"><i class="fas fa-times-circle"></i></button>`)
+
+    $tableRow.appendTo($container);
+    $tableRow.append($column1);
+    $column1.append($productLink);
+    $productLink.append($productImg);
+    $productLink.append($textBox);
+    $textBox.append($productName);
+    $textBox.append($stars);
+
+
+    // $textBox.append($productQuantityAndPrice);
+    // $tableRow.append($delBtn);
+
+    $delBtn.click(() => {
+      this._remove(product.id_product);
+    });
+
+  }
+
   _init(source) {
     this._renderTable();
     this._renderForm();
 
-    // fetch(source)
-    //   .then(result => result.json())
-    //   .then(data => {
-    //     for (let product of data.contents) {
-    //       this.cartItems.push(product);
-    //       this._renderItem(product)
-    //     }
-    //     this.amount = data.amount;
-    //     this.countGoods = data.countGoods;
-    //     this._renderSum();
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //     let $cartProductSection = $('<section class="cart-productSection">В корзине нет товаров</section>');
-    //     $cartProductSection.appendTo($('.cart-sectionsWrapper'));
-    //   })
+    fetch(source)
+      .then(result => result.json())
+      .then(data => {
+        console.log(data);
+        for (let product of data.contents) {
+          this.cartItems.push(product);
+          this._renderItem(product)
+        }
+        this.amount = data.amount;
+        this.countGoods = data.countGoods;
+        this._renderSum();
+      })
+      .catch(error => {
+        console.log(error);
+        let $cartProductSection = $('<section class="cart-productSection">В корзине нет товаров</section>');
+        $cartProductSection.appendTo($('.orderTable-itemsWrapper'));
+      })
   }
 
 }
